4ac7f8193a8fe94f812f0ef02ceff58fcb47eba3
dirodrigueza34 <dianaisabelrodrigueza@gmail.com>
2026-05-09 07:49:56 -0500

    conexion lista

---
+++ Dao/productoDao.java
@@ -0,0 +1,21 @@
+package dao;
+
+import modelo.Producto;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ProductoDao {
+    // 1. Inserción
+    public void registrar(Producto p) {}
+    
+    // 2. Consulta
+    public List<Producto> listar() {}
+    
+    // 3. Actualización
+    public List<Producto> listar() {
+    return new ArrayList<>(); 
+}
+    
+    // 4. Eliminación
+    public void eliminar(int id) {}
+}
---
+++ conexion/conexionBD.java
@@ -0,0 +1,26 @@
+package ConexionBD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexionBD {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/inventario";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection conectar() {
+
+        Connection conexion = null;
+
+        try {
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexion exitosa");
+
+        } catch (Exception e) {
+            System.out.println("Error de conexion: " + e.getMessage());
+        }
+
+        return conexion;
+    }
+}
